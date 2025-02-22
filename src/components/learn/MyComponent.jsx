3b6e11d899e1716eeab899d960be74523a2cd935
Le Son <levanson2004dn@gmail.com>
2025-02-22 08:44:25 +0700

    jsx : cach add style , lay du lieu

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,4 +1,12 @@
+import "./style.css";
 const MyComponent = () => {
-  return <h1> ten toi la Son</h1>;
+  var ten = "son";
+
+  return (
+    <>
+      <h1 className="name"> ten toi la Son</h1>
+      <h1 style={{ color: "red" }}>Le Van {ten}</h1>
+    </>
+  );
 };
 export default MyComponent;
