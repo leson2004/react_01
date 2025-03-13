ccede7147a70c37184b6c9969558ed21f4a89c67
Le Son <levanson2004dn@gmail.com>
2025-03-13 09:10:46 +0700

    bai 67: state hoa form

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,25 +1,42 @@
 import { Input, Button } from "antd";
+import { useState } from "react";
 const UserForm = () => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [passWord, setPassWord] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handleClickBtn = () => {
+    console.log("check_State", { fullName, email, passWord, phoneNumber });
+    // return {
+    //   fullName: fullName,
+    //   email: email,
+    //   passWord: passWord,
+    //   phoneNumber: phoneNumber,
+    // };
+  };
   return (
     <div>
       <div>
         <span>Full Name</span>
-        <Input />
+        <Input onChange={(event) => setFullName(event.target.value)} />
       </div>
       <div>
         <span>Email</span>
-        <Input />
+        <Input onChange={(event) => setEmail(event.target.value)} />
       </div>
       <div>
         <span>Pass Word </span>
-        <Input.Password />
+        <Input.Password onChange={(event) => setPassWord(event.target.value)} />
       </div>
       <div>
         <span>Phone Number </span>
-        <Input />
+        <Input onChange={(event) => setPhoneNumber(event.target.value)} />
       </div>
       <div>
-        <Button type="primary">Submit</Button>
+        <Button type="primary" onClick={() => handleClickBtn()}>
+          Submit
+        </Button>
       </div>
     </div>
   );
